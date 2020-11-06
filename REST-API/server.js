61c2d38488b7fe015d9dca255181c8dc8b7fe07d
Vijaya Kumar V <17252359+vulchivijay@users.noreply.github.com>
2020-11-06 23:39:25 +0530

    post added

--- REST-API/server.js
+++ REST-API/server.js
@@ -13,6 +13,17 @@ app.use(bodyParser.json());
 app.use(cors({ origin: /http:\/\/localhost/ }));
 app.options('*', cors());
 
+// sample database
+const db = {
+  "vulchivijay": {
+    "name": "Vijaya Kumar",
+    "currency": "Rs",
+    "balance": 10000,
+    "description": "Saving account",
+    "transactions": []
+  }
+}
+
 // Configure routes
 const router = express.Router();
 
@@ -23,6 +34,65 @@ router.get("/", (req, res) => {
   res.send(`${package.description} - version ${package.version}`);
 })
 
+router.get("/accounts/:user", (req, res) => {
+  const user = req.params.user;
+  const account = db[user];
+  if (!account) {
+    return res
+              .status(404)
+              .json({error: "User does not exit!", user: `${user}`});
+  } else {
+    return res.json(account);
+  }
+});
+
+router.post("/accounts/", (req, res) => {
+  const body = req.body;
+  // validate required values
+  if (!body.user || !body.currency) {
+    return res
+              .status(400)
+              .json({error: "User and current are required!"});
+  }
+
+  // check account does not exit
+  if (db[body.user]) {
+    return res
+              .status(400)
+              .json({error: "Account already exists!"});
+  }
+
+  // balance
+  let balance = body.balance;
+  if (balance && typeof(balance) !== "number") {
+    balance = parseFloat(balance);
+    if (isNaN(balance)) {
+      return res
+                .status(400)
+                .json({error: "Balance must be a number!"});
+    }
+  }
+
+  // create the account!
+  const account = {
+    user: body.user,
+    currency: body.currency,
+    description: body.description || `${body.user}'s account`,
+    balance: body.balance || 0,
+    transactions: []
+  };
+
+  // return account
+  return res
+            .status(201)
+            .json(account);
+})
+
 app.listen(port, () => {
   console.log("Server up!");
 });
+
+/*
+{ "Content-Type": "application/json" }
+{ "user": "Raju", "currency": "USD", "balance": 100, "transactions": []}
+ */
